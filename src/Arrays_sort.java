6bfe59d4f1bfc50147a28f8551c48455cd59c513
ZHQ <408228147@qq.com>
2017-07-01 10:44:27 +0800

    JavaStudy

--- src/Arrays_sort.java
+++ src/Arrays_sort.java
@@ -34,7 +34,8 @@ public class Arrays_sort {
              ) {
             System.out.println(r);
         }*/
-     array();
+     //array();
+        balances();
     }
 public static void array(){
 
@@ -47,7 +48,52 @@ public static void array(){
 Arrays.binarySearch(lucknumber,3);//二分搜索算法查找值3，找到返回该值得下标，否则返回负数
 
 }
+public static  void balances (){
+    final double STARTRATE = 10;
+    final int NRATES = 6;//6列
+    final int NYEARS = 10;
+    //set interest rates to 10...15%
+    double[] interestRate = new double[NRATES];
+    for (int j = 0; j< interestRate.length; j++)
+        interestRate[j] = (STARTRATE + j)/100;//汇率列
 
+    double[][] balances = new double[NYEARS][NRATES];//6列10行的二维数组
+    //set inital balaces to 10000
+    for (int j = 0;j<balances[0].length; j++)//第一行的值都为10000.00
+      balances[0][j] = 10000;
 
+    //compute interest for future years
+    for (int i= 1; i<balances.length; i++)
+    {
+        for (int j = 0; j<balances[i].length;j++)
+        {
+        //get last year's balance from previous row
+            double oldBalance = balances[i-1][j];
+            //compute interest
+          double interest = oldBalance*interestRate[j];
+          //compute this year's balance
+            balances[i][j] = oldBalance + interest;
+        }
+        }
+
+        //print one row of interest rates
+    for (int j = 0; j< interestRate.length; j++)
+        System.out.printf("%9.0f%%",100*interestRate[j]);
+
+    System.out.println();
+    // print balance table
+    for (double[] row:balances
+         ) {
+          //print table row
+        for (double b: row
+             )
+            System.out.printf("%10.2f",b);
+            System.out.println();
+
+    }
+    }
 
 }
+
+
+
